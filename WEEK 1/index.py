dd6c9186f188631a9ccef1ee0aa55939712783b2
Faith Wanjiiku <wanjikufayie1@gmail.com>
2025-08-07 11:33:55 +0300

    Add files via upload

---
+++ WEEK 1/index.py
@@ -0,0 +1,8 @@
+name = "John Doe"
+
+age = 30
+
+print(name)
+print("my name is Faith Wanjiku")
+print("my age is", age)
+
---
+++ WEEK 1/session 2.py
@@ -0,0 +1,30 @@
+basket = 10.5
+bucket = "water"
+my_name = "Faith Wanjiku - Kenya"
+
+print("This class is crazy")
+print("My bananas are", basket, "and my bucket has", bucket)
+print(my_name)
+
+# 3b + 2c = 10d
+
+jsDaddie = "jsmammie1", "jsmammie2", "jsmammie3"
+paulo = ("software engineer", 10, "paulo3")
+
+# Dictionary Data Type
+
+meseret = {
+    "name": "Meseret",
+    "age": 25,
+    "country": "Ethiopia"
+    "fav_drink": "coffee"
+}
+
+# Boolean Data Type
+
+last_born = True
+
+# set Data Type
+my_basket = {"apple", "banana", "orange"}
+# Tuple Data Type
+my_tuple = ("apple", "banana", "orange")
